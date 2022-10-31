d2883f6826f25119c22a84fb46d41838aa7585f3
Lakei87 <monastyrskyis@gmail.com>
2022-10-31 18:34:03 +0100

    add notification for empty phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,26 @@
-// import { useSelector, useDispatch } from 'react-redux';
-// import { getContacts, filterContacts } from 'redux/contactsSlice';
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/contactsSlice';
 // import { getFilter } from 'redux/filterSlice';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
+import Notification from './Notification';
 import Filter from './Filter';
 import { Box } from './Box';
 
 export default function App() {
+  const contacts = useSelector(getContacts);
+
   return (
     <Box textAlign='center'>
       <h1>Phonebook</h1>
       <ContactForm/>
       <h2>Contacts</h2>
-      <Filter/>
-      <ContactList/>
+      {contacts.length === 0
+        ? (<Notification message={'Phonebook is empty. Please add new contact.'} />)
+        : <>
+          <Filter/>
+          <ContactList/>
+        </>}
     </Box>
   );
 };
