b9b9a401e2581787fc92bc457c50142c3ed6b0a4
Serhii Lakei <93615650+Lakei87@users.noreply.github.com>
2022-11-15 09:52:25 +0100

    add Register page

---
+++ src/components/RegisterForm/index.jsx
@@ -0,0 +1,22 @@
+
+export default function RegisterForm() {
+  return (
+    <div>
+        <form autoComplete="off">
+        <label>
+            Username
+            <input type="text" name="name" />
+        </label>
+        <label>
+            Email
+            <input type="email" name="email" />
+        </label>
+        <label>
+            Password
+            <input type="password" name="password" />
+        </label>
+        <button type="submit">Register</button>
+        </form>
+    </div>
+  )
+}
